db50d5895f39a5ced84be3c6d7d3b4f8f9aeeb7f
Omar Mattos <omarmatt84@gmail.com>
2025-07-21 20:29:50 -0300

    creamos la estructura del proyecto, limpiamos y agregamos algunos componentes, instalamos las dependencias

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Menu from "./components/shared/Menu"
+import Footer from "./components/shared/Footer"
+import Index from "./components/pages/Index"
+
+function App() {
+
+  return (
+    <>
+      <Menu></Menu>
+      <main>
+        <Index></Index>
+      </main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
