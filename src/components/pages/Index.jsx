0083e0ed47cfbaf77b233e8eb1c67531fbf15a1f
Omar Mattos <omarmatt84@gmail.com>
2025-07-22 17:51:35 -0300

    realice el maquetado de la pagina de inicio, importé el componente CardCancion y agregue estilos en index.css

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,9 +1,34 @@
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import CardCancion from "./cancion/CardCancion";
 
 const Index = () => {
     return (
-        <div>
-            <h1>Hola Mundo</h1>
-        </div>
+        <>
+        <hr />
+        <section className="mainSection bg-index">
+            <Container className="mt-5">
+                <Form inline>
+                        <Row className="d-flex justify-content-between">
+                            <Col xs="auto">
+                                <h3 className="fw-light fs-3">Top Hits</h3>
+                            </Col>
+                            <Col xs="auto d-flex">
+                                <i className="bi bi-search fs-3 me-2 text-secondary"></i>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Buscar canciones"
+                                    className=" mr-sm-2"
+                                />
+                            </Col>
+                        </Row>
+                    </Form>
+                <hr />
+                <Row>
+                    <CardCancion></CardCancion>
+                </Row>
+            </Container>
+        </section>
+        </>
     );
 };
 
