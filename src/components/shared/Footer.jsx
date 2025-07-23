5e5f942d8908199505eedc194adc5cb5f9db0811
Santiago <concursos.utn.frt@gmail.com>
2025-07-23 20:53:16 -0300

    termine de hacer el footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,32 +1,37 @@
 import { Link } from "react-router";
 import badgeapple from "../../assets/badge-apple-store.svg"
 import googleplay from "../../assets/google-play-badge-bb.svg"
+import echoLogo from "../../assets/echoTune_Logo-negativo.png"
 
 const Footer = () => {
     return (
- <footer className="py-4 bg-info">
+ <footer className=" bg-dark-subtle py-3 pe-3 ps-3">
     <div className="row">
-        <h2 className=" text-light text-center fs-1">EchoTune</h2>
-         <div className="col-12 col-md-6 col-lg-4 text-center border border-danger">
-                    <h5 className="text-light">Mobile</h5>
-                    <div className="d-flex">
+        <div className="col-12 col-md-12 col-lg-12 text-center">
+            <Link className="link"><img src={echoLogo} alt="logotipo" className="img-fluid logotipo"/></Link>
+        </div>
+         <div className="col-12 col-md-6 col-lg-4 text-center ps-5">
+                    <h4 className="text-light">Disponible en mobiles</h4>
+                    <div className="d-flex justify-content-center gap-2">
+                        <Link className="link"><img src={badgeapple} alt="appstore" className=" img-fluid appstore"/></Link>
+                        <Link className="link"><img src={googleplay} alt="playstore" className="img-fluid playstore"/></Link>
                     </div>
-         </div>
-                <div className="col-12 col-md-6 col-lg-4 text-center border border-danger">
-                    <ul className="list-unstyled text-light text-light">
-                        <li><Link className="link">Inicio</Link></li>
-                        <li><Link className="link">Nosotros</Link></li>
-                        <li><Link className="link"></Link></li>
-                        <li><Link className="link">Iniciar Sesión</Link></li>
+          </div>
+                <div className="col-12 col-md-6 col-lg-4 text-center">
+                    <ul className="list-unstyled">
+                        <Link className="link text-light fs-5"><li>Inicio</li></Link>
+                       <Link className="link text-light fs-5"> <li>Nosotros</li></Link>
+                        <Link className="link text-light fs-5"><li>Registrarse</li></Link>
+                       <Link className="link text-light fs-5"> <li>Iniciar Sesión</li></Link>
                     </ul>
                 </div>
-                <div className="col-12 col-md-6 col-lg-4 text-center border border-danger">
-                <h5 className="text-light">Seguinos</h5>
-                <div className="d-flex justify-content-center">
-                    <i className="bi bi-twitter-x text-light fs-3"></i>
-                    <i className="bi bi-facebook text-primary fs-3"></i>
-                    <i className="bi bi-instagram fs-3 text-instagram"></i>
-                </div>
+                <div className="col-12 col-md-12 col-lg-4 text-center pe-5">
+                <h4 className="text-light my-2">Seguinos en nuestras redes</h4>
+                <div className="d-flex justify-content-center gap-3">
+                    <Link className="link text-light"><i className="bi bi-twitter-x text-light fs-2"></i></Link>
+                    <Link className="link text-light"><i className="bi bi-facebook color-facebook fs-2"></i></Link>
+                    <Link className="link text-light"><i className="bi bi-instagram fs-2 color-instagram"></i></Link>
+                    </div>
                 </div>
                 </div>
         </footer>
