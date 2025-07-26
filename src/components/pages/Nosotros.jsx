33c1df21b594b81c35b36a96c452066bb8ef8d2b
Santiago <concursos.utn.frt@gmail.com>
2025-07-26 20:37:47 -0300

    terminando de realizar el maquetado del componente nosotros

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -1,36 +1,33 @@
 import { Card, Col, Container, Row } from "react-bootstrap";
-
+import santigonz from "../../assets/f9eb2834-675e-4536-a80e-ba544d4f34a6.jpg"
 
 const Nosotros = () => {
     return (
         <Container>
             <h1 className='text-center'>Acerca de Nosotros</h1>
-            <h3 className="text-center">"Creamos la banda sonora de tu vida" o "Sinfonías en cada click"</h3>
+            <h3 className="text-center my-4">"Con pasión y código, nuestro equipo de desarrolladores construyó una página de música donde cada acorde y melodía cobra vida."</h3>
             <Row>
-                <Col md={4} lg={4}>
-                <Card className="h-100">
-                    <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?_gl=1*l4v59o*_ga*MTc3MDA1MjEyMS4xNzQxNzIzMDE3*_ga_8JE65Q40S6*czE3NTMzOTcyMzYkbzckZzEkdDE3NTMzOTcyNjkkajI3JGwwJGgw" alt="stefan stefancik" className="img-fluid h-100 w-100"/>
+                <Col md={4} lg={4} className="my-4">
+                <Card className="h-100 rounded-2">
+                    <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?_gl=1*l4v59o*_ga*MTc3MDA1MjEyMS4xNzQxNzIzMDE3*_ga_8JE65Q40S6*czE3NTMzOTcyMzYkbzckZzEkdDE3NTMzOTcyNjkkajI3JGwwJGgw" alt="stefan stefancik" className="img-fluid h-100 w-100 rounded-3"/>
                     <Card.Body>
-                        <Card.Text>Stefan Stefancik</Card.Text>
-                        <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repellendus consequatur veniam!</Card.Text>
+                        <Card.Title className="text-center"><h4>Omar Mattos</h4></Card.Title>
                     </Card.Body>
                 </Card>
                 </Col>
-                <Col md={4} lg={4}>
-                <Card className="h-100">
-                    <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?_gl=1*1xym2sn*_ga*MTc3MDA1MjEyMS4xNzQxNzIzMDE3*_ga_8JE65Q40S6*czE3NTMzOTcyMzYkbzckZzEkdDE3NTMzOTcyODgkajgkbDAkaDA." alt="justin shaifer" className="img-fluid h-100 w-100"/>
+                <Col md={4} lg={4} className="my-4">
+                <Card className="h-100 rounded-2">
+                    <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?_gl=1*1xym2sn*_ga*MTc3MDA1MjEyMS4xNzQxNzIzMDE3*_ga_8JE65Q40S6*czE3NTMzOTcyMzYkbzckZzEkdDE3NTMzOTcyODgkajgkbDAkaDA." alt="justin shaifer" className="img-fluid h-100 rounded-3"/>
                     <Card.Body>
-                        <Card.Text>Justin Shaifer</Card.Text>
-                        <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repellendus consequatur veniam!</Card.Text>
+                        <Card.Title className="text-center"><h4>Santiago Lacki</h4></Card.Title>
                     </Card.Body>
                 </Card>
                 </Col>
-                <Col md={4} lg={4}>
-                <Card className="h-100">
-                    <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?_gl=1*kts5kc*_ga*MTc3MDA1MjEyMS4xNzQxNzIzMDE3*_ga_8JE65Q40S6*czE3NTMzOTcyMzYkbzckZzEkdDE3NTMzOTczMDckajU2JGwwJGgw" alt="simon robben" className="img-fluid h-100"/>
+                <Col md={4} lg={4} className="my-4">
+                <Card className="h-100 rounded-2">
+                    <img src={santigonz} alt="simon robben" className="img-fluid h-100 rounded-3"/>
                     <Card.Body>
-                        <Card.Text>Stefan Stefancik</Card.Text>
-                        <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repellendus consequatur veniam!</Card.Text>
+                        <Card.Title className="text-center"><h4>Santiago Gonzalez</h4></Card.Title>
                     </Card.Body>
                 </Card>
                 </Col>
