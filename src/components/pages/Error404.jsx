db5436ff675c0a4eaef5758c9707078967bc0fd4
Santiago Lacki <santiagolacki@gmail.com>
2025-07-27 20:27:29 -0300

    Se agrego el contenido de la pagina de error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,10 +1,22 @@
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router';
 
-const Error404 = () => {
-    return (
-        <div>
-            <h1>Error404</h1>
-        </div>
-    );
+const error404 = () => {
+  return (
+      <Container className="d-flex flex-column justify-content-center text-center min-vh-100">
+        <Row>
+          <Col>
+            <h1 className="display-1 fw-bold mb-2 text-">404 Not Found...</h1>
+            <p className="fs-3">
+              <span className="text-secondary">¡Oops! Página no encontrada.</span>
+            </p>
+            <Button as={Link} to="/" variant="secondary" size="lg">
+              Volver al Inicio
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+  );
 };
 
-export default Error404;
+export default error404;
