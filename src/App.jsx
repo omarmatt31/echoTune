4e56f7c2a694ddbe9ea76363856a232d954971d1
Santiago <concursos.utn.frt@gmail.com>
2025-07-27 21:03:57 -0300

    realize la logica del componente inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Menu from "./components/shared/Menu"
 import Footer from "./components/shared/Footer"
-import Index from "./components/pages/Index"
 import DetalleCancion from "./components/pages/DetalleCancion";
 import Login from "./components/pages/Login";
 import Administrador from "./components/pages/Administrador";
 import Nosotros from "./components/pages/Nosotros";
 import FormularioCancion from "./components/pages/cancion/FormularioCancion";
 import Error404 from "./components/pages/Error404";
+import Inicio from "./components/pages/Inicio";
 
 function App() {
 
@@ -17,7 +17,7 @@ function App() {
       <Menu></Menu>
       <main>
         <Routes>
-          <Route path="/" element={<Index></Index>}></Route>
+          <Route path="/" element={<Inicio></Inicio>}></Route>
           <Route path="/detalle" element={<DetalleCancion></DetalleCancion>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/administrador" element={<Administrador></Administrador>}></Route>
---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,51 @@
+import { Container, Form, Row, Col} from "react-bootstrap";
+import CardCancion from "./cancion/CardCancion";
+import { useState } from "react";
+
+const Inicio = ({canciones}) => {
+    const [terminoBusqueda, setTerminoBusqueda] = useState('')
+
+    const handleBuscarChange=(e)=>{
+        setTerminoBusqueda(e.target.value)
+    }
+
+    const cancionesBuscadas= canciones.filter((cancion)=>cancion.titulo.toLowerCase().includes(terminoBusqueda.toLocaleLowerCase()))
+    
+    return (
+        <>
+        <section className="mainSection bg-index">
+            <Container className="mt-5">
+                <Form>
+                        <Row className="d-flex justify-content-between">
+                            <Col xs="auto">
+                                <h3 className="fw-light fs-3 color-secundario">Top Hits</h3>
+                            </Col>
+                            <Col xs="auto d-flex">
+                                <i className="bi bi-search fs-3 me-2 text-secondary"></i>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Buscar canciones"
+                                    className=" mr-sm-2"
+                                    onChange={handleBuscarChange}
+                                    value={terminoBusqueda}
+                                />
+                            </Col>
+                        </Row>
+                    </Form>
+                <hr />
+                <Row>
+                    {
+                    cancionesBuscadas.length > 0 ? (
+                    cancionesBuscadas.map((cancion)=><CardCancion key={cancion.id} cancion={cancion}></CardCancion>)
+                    ) : (
+                        <p>No se encontraron canciones para mostrar</p>
+                    )
+                    }
+                </Row>
+            </Container>
+        </section>
+        </>
+    );
+};
+
+export default Inicio;
